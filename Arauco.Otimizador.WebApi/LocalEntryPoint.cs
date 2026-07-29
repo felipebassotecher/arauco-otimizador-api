c3a4b904f001e43f42adff250bbd26b2ac1c5f2c
Felipe Basso <felipe.basso@techer.com.br>
2026-07-29 17:51:32 -0300

    feat: ajuste entry point da api

--- Arauco.Otimizador.WebApi/LocalEntryPoint.cs
+++ Arauco.Otimizador.WebApi/LocalEntryPoint.cs
@@ -1,4 +1,7 @@
-﻿namespace Arauco.Otimizador.WebApi;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Arauco.Otimizador.WebApi;
 
 /// <summary>
 /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
