77b3ba32fcb7fce61820e0a81a01e664b47a9b14
Felipe Basso <felipe.basso@techer.com.br>
2026-07-29 03:09:36 -0300

    feat: remocao referencias - colaborador

--- Arauco.Otimizador.Common.Domain/Models/Colaborador/ColaboradorListaModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Arauco.Otimizador.Common.Domain.Models.Colaborador;
-
-public class ColaboradorListaModel
-{
-    public int Key { get; set; }
-    public string Value { get; set; }
-}
--- Arauco.Otimizador.Common.Domain/Models/Conta/ProfileModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Arauco.Otimizador.Common.Domain.Models.Conta;
-
-public class ProfileModel
-{
-    public int ColaboradorId { get; set; }
-    public string Nome { get; set; }
-}
